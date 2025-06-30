f2fb05298f96497795c64b9c4280571a41cb8fdc
Gustavo Fring <gustavogusfring123@gmail.com>
2025-06-30 23:00:58 +0400

    Added Project Template

---
+++ GeoTrip/Data/DishContext.cs
@@ -0,0 +1,42 @@
+﻿using GeoTrip.Models;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoTrip.Data
+{
+    public class DishContext : DbContext
+    {
+        public DishContext(DbContextOptions<DishContext> options) : base(options)
+        {
+        }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dish>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Price)
+                    .HasColumnType("decimal(10,2)")
+                    .IsRequired();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ImageUrl)
+                  .HasMaxLength(8000);
+            });
+        }
+    }
+}
